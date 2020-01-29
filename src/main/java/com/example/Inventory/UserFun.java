6da441fd066463a58a7e540722316d11ce9b1ffc
havya-1310 <hkpanchal1998@gmail.com>
2020-01-29 15:36:23 +0530

    Added function to generate random string

--- src/main/java/com/example/Inventory/UserFun.java
+++ src/main/java/com/example/Inventory/UserFun.java
@@ -28,7 +28,17 @@ public class UserFun {
             return new User("", "", "");
         }
     }
-
+      protected String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+    }
     public List<User> getAllUsers() {
         return mongoTemplate.findAll(User.class);
     }
